be364b74749dccd83ed8622e4c3967681cf0a45b
Наталья Беляева <kasyanka@bk.ru>
2019-07-19 22:07:56 +0300

    пока все дома. Часть 2

--- js/card.js
+++ js/card.js
@@ -10,9 +10,72 @@ window.renderShowCard = function (data) {
                   .querySelector('.map__card');
     var element = template.cloneNode(true);
     element.classList.add('www');
+
     var title = element.querySelector('.popup__title');
+    var address = element.querySelector('.popup__text--address');
+    var price = element.querySelector('.popup__text--price');
+    var type = element.querySelector('.popup__type');
+    var capacity = element.querySelector('.popup__text--capacity');
+    var time = element.querySelector('.popup__text--time');
+    var features = element.querySelector('.popup__features');
+    var wifi = features.querySelector('.popup__feature--wifi');
+    var dishwasher = features.querySelector('.popup__feature--dishwasher');
+    var parking = features.querySelector('.popup__feature--parking');
+    var washer = features.querySelector('.popup__feature--washer');
+    var elevator = features.querySelector('.popup__feature--elevator');
+    var conditioner = features.querySelector('.popup__feature--conditioner');
+    var description = element.querySelector('.popup__description');
+    var avatar = element.querySelector('.popup__avatar');
+    // var photoBlock = element.querySelector('.popup__photos');
+    // var photoImg = element.querySelector('.popup__photo');
 
     title.textContent = data[n].offer.title;
+    address.textContent = data[n].offer.address;
+    price.textContent = data[n].offer.price + '₽/ночь';
+    type.textContent = data[n].offer.type; // доделать
+    capacity.textContent = data[n].offer.rooms + ' комнаты для ' + data[n].offer.guests + ' гостей';
+    time.textContent = 'Заезд после ' + data[n].offer.checkin + ', ' + 'выезд до ' + data[n].offer.checkout;
+    description.textContent = data[n].offer.description;
+    avatar.src = data[n].author.avatar;
+
+    var renerType = function (english, russian) {
+      if (data[n].offer.type.indexOf(english) !== -1) {
+        type.textContent = russian;
+      }
+    };
+    renerType('palace', 'Дворец');
+    renerType('flat', 'Квартира');
+    renerType('house', 'Дом');
+    renerType('bungalo', 'Бунгало');
+
+    var photoBlock = element.querySelector('.popup__photos');
+    var photoImg = element.querySelector('.popup__photo');
+
+    var photosSum = data[n].offer.photos;
+    for (var i = 0; i < photosSum.length; i++) {
+      photoBlock.appendChild(photoImg.cloneNode(true));
+      photoImg.src = data[n].offer.photos[i];
+    }
+    photoBlock.removeChild(photoImg);
+
+    if (data[n].offer.features.indexOf('wifi') === -1) {
+      features.removeChild(wifi);
+    }
+    if (data[n].offer.features.indexOf('dishwasher') === -1) {
+      features.removeChild(dishwasher);
+    }
+    if (data[n].offer.features.indexOf('parking') === -1) {
+      features.removeChild(parking);
+    }
+    if (data[n].offer.features.indexOf('washer') === -1) {
+      features.removeChild(washer);
+    }
+    if (data[n].offer.features.indexOf('elevator') === -1) {
+      features.removeChild(elevator);
+    }
+    if (data[n].offer.features.indexOf('conditioner') === -1) {
+      features.removeChild(conditioner);
+    }
 
     fragment.appendChild(element);
     return fragment;
@@ -22,12 +85,17 @@ window.renderShowCard = function (data) {
     var map = document.querySelector('.map');
     var block = document.querySelector('.map__filters-container');
     var mapPinAll = document.querySelectorAll('.pin_all');
+    var templateCard = document.querySelector('.map__card');
+
     for (var i = 0; i < mapPinAll.length; i++) {
-      var j = i;
-      mapPinAll[i].addEventListener('click', function () {
-        console.log(j)
-        map.insertBefore(getCard(j), block);
-      });
+      (function (j) {
+        mapPinAll[j].addEventListener('click', function () {
+          if (templateCard) {
+            map.removeChild(templateCard);
+          }
+          map.insertBefore(getCard(j), block);
+        });
+      })(i);
     }
   };
   showCard();

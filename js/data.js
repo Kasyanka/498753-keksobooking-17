60ae0aadbdc349282f99826874205014cfc2ce1b
Наталья Беляева <kasyanka@bk.ru>
2019-07-22 22:16:13 +0300

    22 задание готово полностью

--- js/data.js
+++ js/data.js
@@ -46,29 +46,96 @@ window.renderPinsClick = function () {
   };
 
   var housingType = document.querySelector('#housing-type'); // селект тип жилья
+  var housingRoom = document.querySelector('#housing-rooms'); // селект комнаты
+  var housingGuest = document.querySelector('#housing-guests'); // селект гости
   var housingPrice = document.querySelector('#housing-price'); // селект цена
-  var housingRoom = document.querySelector('#housing-rooms'); // селект цена
-  var housingQuest = document.querySelector('#housing-guests'); // селект цена
+  var inputWifi = document.querySelector('#filter-wifi');
+  var inputDishwasher = document.querySelector('#filter-dishwasher');
+  var inputParking = document.querySelector('#filter-parking');
+  var inputWasher = document.querySelector('#filter-washer');
+  var inputElevator = document.querySelector('#filter-elevator');
+  var inputConditioner = document.querySelector('#filter-conditioner');
 
-  var filteringType = function (type, room, array) {
+
+
+  var filteringType = function (type, room, guest, price, wifi, dishwasher, parking, washer, elevator, conditioner, array) {
     window.SERVER_DATA = array;
-
+    console.log(conditioner);
     var filterPinType = window.SERVER_DATA.filter(function (objeckt) {
-      return objeckt.offer.type === type;
+      return type === 'any' || objeckt.offer.type === type;
     });
-
-    console.log(filterPinType)
 
     var filterPinRooms = filterPinType.filter(function (objeckt) {
-      return objeckt.offer.rooms === room;
+      return room === 'any' || objeckt.offer.rooms === parseInt(room, 10);
     });
 
-    console.log(room)
+    var filterPinQuest = filterPinRooms.filter(function (objeckt) {
+      return guest === 'any' || objeckt.offer.guests === parseInt(guest, 10);
+    });
 
-    console.log(filterPinRooms)
+    if (price === 'any') {
+      var filterPinPrice = filterPinQuest;
+    }
+    if (price === 'low') {
+      filterPinPrice = filterPinQuest.filter(function (objeckt) {
+        return objeckt.offer.price < 10000;
+      });
+    }
+    if (price === 'high') {
+      filterPinPrice = filterPinQuest.filter(function (objeckt) {
+        return objeckt.offer.price >= 50000;
+      });
+    }
+    if (price === 'middle') {
+      filterPinPrice = filterPinQuest.filter(function (objeckt) {
+        return objeckt.offer.price >= 10000 && objeckt.offer.price < 50000;
+      });
+    }
+    if (wifi) {
+      var filterPinWifi = filterPinPrice.filter(function (objeckt) {
+        return objeckt.offer.features.indexOf('wifi') !== -1;
+      });
+    } else {
+      filterPinWifi = filterPinPrice;
+    }
+    if (dishwasher) {
+      var filterPinDishwasher = filterPinWifi.filter(function (objeckt) {
+        return objeckt.offer.features.indexOf('dishwasher') !== -1;
+      });
+    } else {
+      filterPinDishwasher = filterPinWifi;
+    }
+    if (parking) {
+      var filterPinParking = filterPinDishwasher.filter(function (objeckt) {
+        return objeckt.offer.features.indexOf('parking') !== -1;
+      });
+    } else {
+      filterPinParking = filterPinDishwasher;
+    }
+    if (washer) {
+      var filterPinWasher = filterPinParking.filter(function (objeckt) {
+        return objeckt.offer.features.indexOf('washer') !== -1;
+      });
+    } else {
+      filterPinWasher = filterPinParking;
+    }
+    if (elevator) {
+      var filterPinElevator = filterPinWasher.filter(function (objeckt) {
+        return objeckt.offer.features.indexOf('elevator') !== -1;
+      });
+    } else {
+      filterPinElevator = filterPinWasher;
+    }
+    if (conditioner) {
+      var filterPinConditioner = filterPinElevator.filter(function (objeckt) {
+        return objeckt.offer.features.indexOf('conditioner') !== -1;
+      });
+    } else {
+      filterPinConditioner = filterPinElevator;
+    }
 
-    renderPins(filterPinRooms.slice(0, 5));
-    window.renderShowCard(filterPinRooms.slice(0, 5));
+    renderPins(filterPinConditioner.slice(0, 5));
+    window.renderShowCard(filterPinConditioner.slice(0, 5));
   };
 
   var filteringPins = function (array) {
@@ -84,16 +151,30 @@ window.renderPinsClick = function () {
         mapPins.removeChild(mapPin[i]);
       }
       window.closeCard();
-      if (housingType.value === 'any' && housingPrice.value === 'any' && housingRoom.value === 'any' && housingQuest.value === 'any') {
+      if (housingType.value === 'any' && housingPrice.value === 'any' && housingRoom.value === 'any' && housingGuest.value === 'any' && inputWifi.checked === false && inputDishwasher.checked === false && inputParking.checked === false && inputWasher.checked === false && inputElevator.checked === false && inputConditioner.checked === false) {
         renderPins(array.slice(0, 5));
         window.renderShowCard(array);
       } else {
-        filteringType(housingType.value, housingRoom.value, array);
+        var lastTimeout;
+        if (lastTimeout) {
+          window.clearTimeout(lastTimeout);
+        }
+        window.setTimeout(function () {
+          lastTimeout = filteringType(housingType.value, housingRoom.value, housingGuest.value, housingPrice.value, inputWifi.checked, inputDishwasher.checked, inputParking.checked, inputWasher.checked, inputElevator.checked, inputConditioner.checked, array);
+        }, 500);
       }
     };
 
     housingType.addEventListener('change', onChangeFilterAll);
     housingRoom.addEventListener('change', onChangeFilterAll);
+    housingGuest.addEventListener('change', onChangeFilterAll);
+    housingPrice.addEventListener('change', onChangeFilterAll);
+    inputWifi.addEventListener('change', onChangeFilterAll);
+    inputDishwasher.addEventListener('change', onChangeFilterAll);
+    inputParking.addEventListener('change', onChangeFilterAll);
+    inputWasher.addEventListener('change', onChangeFilterAll);
+    inputElevator.addEventListener('change', onChangeFilterAll);
+    inputConditioner.addEventListener('change', onChangeFilterAll);
   };
 
   window.backend.load(filteringPins, errorHandler);
